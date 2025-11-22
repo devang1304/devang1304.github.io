c4711dec88246cd77a179c268b208cbf5eb19227
Devang Dhanuka <devangdhanuka@gmail.com>
2025-11-22 01:52:34 -0500

    updating webpage to fun design

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "node:path";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src")
-    }
-  },
-  build: {
-    sourcemap: true
-  }
 });
